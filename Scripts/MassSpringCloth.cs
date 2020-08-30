f150309abef859070bcd8fe0d168d04f6d40d2f8
Guille <guille_elias_alonso@hotmail.com>
2020-08-31 03:24:05 +0800

    Initial commit, base cloth with super elasticity issues.

---
+++ Scripts/MassSpringCloth.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MassSpringCloth : MonoBehaviour
+{
+    public Mesh referenceMesh;
+    public Material clothMaterial;
+    public Material springMaterial;
+    public Material massMaterial;
+    public float mass = 0.05f;
+    public bool drawNet = true;
+    public bool drawMesh = false;
+
+    private Mesh sphereMesh;
+    private Mesh cylinderMesh;
+
+    private Mesh mesh;
+    private Spring[,] springs;
+    private Vector3[] positions;
+    private Vector3[] velocities;
+    private Vector3 gravity = new Vector3(0, -9.8f, 0);
+
+    // Start is called before the first frame update
+    void OnValidate()
+    {
+        Init();
+    }
+
+    void Awake()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Application.isPlaying)
+        {
+            Simulate();
+        }
+        
+        if (drawNet)
+        {
+            DrawNet();
+        }
+        if(drawMesh)
+        {
+            Graphics.DrawMesh(mesh, transform.localToWorldMatrix, clothMaterial, 0);
+        }
+/*
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.delayCall += () =>
+        {
+            UnityEditor.EditorApplication.QueuePlayerLoopUpdate();
+        };
+#endif*/
+    }
+
+    void Init()
+    {
+        mesh = CopyMesh(referenceMesh);
+
+        springs = new Spring[mesh.vertexCount, 4];
+
+        positions = mesh.vertices;
+        velocities = new Vector3[mesh.vertexCount];
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            velocities[i] = Vector3.zero;
+
+        }
+
+        GameObject sphObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        GameObject cylObj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+        sphereMesh = sphObj.GetComponent<MeshFilter>().sharedMesh;
+        cylinderMesh = cylObj.GetComponent<MeshFilter>().sharedMesh;
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.delayCall += () =>
+        {
+#endif
+            GameObject.DestroyImmediate(sphObj);
+            GameObject.DestroyImmediate(cylObj);
+#if UNITY_EDITOR
+        };
+#endif
+    }
+
+    void Simulate()
+    {
+        float time = Time.deltaTime;
+        Vector3 force = gravity * mass;
+        for(int i = 0; i < mesh.vertexCount; i++)
+        {
+            velocities[i] += Time.deltaTime * force / mass;
+            positions[i] += Time.deltaTime * velocities[i];
+        }
+        mesh.vertices = positions;
+    }
+
+    void DrawNet()
+    {
+        Vector3 scale = Vector3.one * 0.05f;
+        Matrix4x4 scaleMat = Matrix4x4.Scale(scale);
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            Graphics.DrawMesh(sphereMesh, Matrix4x4.Translate(mesh.vertices[i]) * transform.localToWorldMatrix * scaleMat, massMaterial, 0);
+        }
+    }
+
+    static Mesh CopyMesh(Mesh mesh)
+    {
+        Mesh newmesh = new Mesh();
+        newmesh.vertices = mesh.vertices;
+        newmesh.triangles = mesh.triangles;
+        newmesh.uv = mesh.uv;
+        newmesh.normals = mesh.normals;
+        newmesh.colors = mesh.colors;
+        newmesh.tangents = mesh.tangents;
+        return newmesh;
+    }
+}
---
+++ Scripts/MassSpringClothRectangle.cs
@@ -0,0 +1,270 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Spring
+{
+    public int[] vertexIndices = new int[2];
+    public float restLength;
+    public float stiffness;
+
+    private MassSpringClothRectangle cloth;
+
+    public Spring(MassSpringClothRectangle cloth, int v1, int v2, float stiffness = 10)
+    {
+        this.vertexIndices[0] = v1;
+        this.vertexIndices[1] = v2;
+        this.cloth = cloth;
+        this.stiffness = stiffness;
+        this.restLength = this.getLength();
+    }
+
+    public float getLength()
+    {
+        return (cloth.getVertex(vertexIndices[0]) - cloth.getVertex(vertexIndices[1])).magnitude;
+    }
+
+    public float CalcSpringForce()
+    {
+        float distFromRest = this.restLength - this.getLength();
+        if (Mathf.Abs(distFromRest) > 0.0001)
+            return stiffness * distFromRest;
+        return 0;
+    }
+
+    public Vector3 CalcSpringForce(int vertex)
+    {
+        Vector3 a = vertex == vertexIndices[0] ? cloth.getVertex(vertexIndices[1]) : cloth.getVertex(vertexIndices[0]);
+        Vector3 b = vertex == vertexIndices[0] ? cloth.getVertex(vertexIndices[0]) : cloth.getVertex(vertexIndices[1]);
+        Vector3 link = b - a;
+        //return this.CalcSpringForce() * (b-a).normalized;
+        Vector3 force = (link - (link / link.magnitude) * this.restLength) * this.stiffness;
+        /*if(force.magnitude < 0.001)
+        {
+            return Vector3.zero;
+        }*/
+        return -force;
+    }
+}
+
+[ExecuteInEditMode, RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MassSpringClothRectangle : MonoBehaviour
+{
+    static float timer = 0.0f;
+
+    private static int SPRINGS_PER_VERTEX = 12;
+    private static int[,] links = {
+        {0, 1},
+        {1, 0},
+        {0, -1},
+        {-1, 0},
+        {1, 1},
+        {-1, 1},
+        {1, -1},
+        {-1, -1},
+        {0, 2},
+        {2, 0},
+        {0, -2},
+        {-2, 0},
+    };
+
+    public int xResolution, yResolution;
+    public int[] fixedVertices;
+    public Material massMaterial;
+    public float mass = 1.0f;
+    public float stiffness;
+    public float damping;
+    public bool isSimulating = false;
+    public bool drawNet = false;
+    public bool addWind = false;
+    public Vector3 windVelocity;
+
+    private Mesh mesh;
+    private Mesh sphereMesh;
+    private Spring[,] springs;
+    private Vector3[] vertices;
+    private Vector3[] velocities;
+    private Vector3 gravity = new Vector3(0, -9.8f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void OnValidate()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (drawNet)
+        {
+            DrawNet();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Application.isPlaying && isSimulating)
+        {
+            Simulate();
+        }
+    }
+
+    void Init()
+    {
+        if (sphereMesh == null)
+        {
+            GameObject sphObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphereMesh = sphObj.GetComponent<MeshFilter>().sharedMesh;
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.delayCall += () =>
+            {
+#endif
+                GameObject.DestroyImmediate(sphObj);
+#if UNITY_EDITOR
+            };
+#endif
+        }
+
+        if (vertices != null && xResolution * yResolution > 0 && xResolution * yResolution == vertices.Length)
+        {
+            return;
+        }
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Cloth mesh";
+
+        // 顶点
+        vertices = new Vector3[xResolution * yResolution];
+        Vector2[] uv = new Vector2[vertices.Length];
+        for (int i = 0, y = 0; y < yResolution; y++)
+        {
+            for (int x = 0; x < xResolution; x++, i++)
+            {
+                vertices[i] = new Vector3((float)x / (xResolution - 1) - 0.5f, (float)y / (yResolution - 1) - 0.5f);
+                uv[i] = new Vector2((float)x / xResolution, (float)y / yResolution);
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+
+        // 三角形
+        int[] triangles = new int[(xResolution - 1) * (yResolution - 1) * 6];
+        for (int ti = 0, vi = 0, y = 0; y < yResolution - 1; y++, vi++)
+        {
+            for (int x = 0; x < xResolution - 1; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xResolution;
+                triangles[ti + 5] = vi + xResolution + 1;
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        velocities = new Vector3[mesh.vertexCount];
+        
+        // 初始化弹簧
+        int springcount = 0;
+        springs = new Spring[mesh.vertexCount, SPRINGS_PER_VERTEX];
+        for (int i = 0, y = 0; y < yResolution; y++)
+        {
+            for (int x = 0; x < xResolution; x++, i++)
+            {
+                int j = 0;
+                for(int k = 0; k < SPRINGS_PER_VERTEX; k++)
+                {
+                    Vector2Int xy2 = new Vector2Int(x, y);
+                    xy2 += new Vector2Int(links[k, 0], links[k, 1]);
+                    if(xy2.x < 0 || xy2.y < 0 || xy2.x >= xResolution || xy2.y >= yResolution)
+                    {
+                        continue;
+                    }
+                    int i2 = xy2.y * xResolution + xy2.x;
+                    if(i2 > i)
+                    {
+                        springs[i, j] = new Spring(this, i, i2, stiffness);
+                        springcount++;
+                    } else
+                    {
+                        springs[i, j] = GetSpring(i2, i);
+                    }
+                    
+                    j++;
+                }
+            }
+        }
+    }
+
+    void DrawNet()
+    {
+        Vector3 scale = Vector3.one * 0.05f;
+        Matrix4x4 scaleMat = Matrix4x4.Scale(scale);
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            Graphics.DrawMesh(sphereMesh, Matrix4x4.Translate(mesh.vertices[i]) * transform.localToWorldMatrix * scaleMat, massMaterial, 0);
+        }
+    }
+
+    void Simulate()
+    {
+        timer += Time.deltaTime;
+        /*float time = Time.deltaTime;
+        if(time > 0.02f)
+        {
+            time = 0.02f;
+        }*/
+        Vector3 gravForce = gravity * mass;
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            if(!fixedVertices.Contains(i))
+            {
+                Vector3 accForce = gravForce; // 重力
+                accForce += -damping * velocities[i]; // 阻尼
+                for(int s = 0; s < SPRINGS_PER_VERTEX; s++)
+                {
+                    if(springs[i,s] != null)
+                    {
+                        //Vector3 sf = springs[i, s].CalcSpringForce(i);
+                        accForce += springs[i, s].CalcSpringForce(i);
+                    }
+                }
+                //accForce /= 100;s
+                Vector3 viscousFluidForce;
+                if (addWind)
+                    viscousFluidForce = mesh.normals[i] * (Vector3.Dot(mesh.normals[i], windVelocity - velocities[i])) * 1.0f;
+                else
+                    viscousFluidForce = Vector3.zero;
+                accForce += viscousFluidForce;
+
+                velocities[i] += Time.deltaTime * accForce / mass;
+                vertices[i] += Time.deltaTime * velocities[i];
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+
+    public Vector3 getVertex(int vi)
+    {
+        return this.vertices[vi];
+    }
+
+    public Spring GetSpring(int v1, int v2)
+    {
+        for(int i = 0; i < SPRINGS_PER_VERTEX; i++)
+        {
+            if(springs[v1, i] != null && springs[v1, i].vertexIndices[1] == v2)
+            {
+                return springs[v1, i];
+            }
+        }
+        return null;
+    }
+}
